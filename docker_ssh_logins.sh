bf827058bb99a13bc5bf787b7d0c4890b5a8ead0
Tamin Mahboubi <taminmahboubi@gmail.com>
2025-02-10 14:14:52 +0000

    Updated Code: clearer output

--- docker_ssh_logins.sh
+++ docker_ssh_logins.sh
@@ -3,22 +3,19 @@
 # Array of Docker container names for SSH connection
 containers=("managed-node1" "managed-node2" "managed-node3")
 
-# Function to check if Docker container is running, and start if necessary
-check_and_start_container(){
-	container=$1
-	status=$(docker inspect -f '{{.State.Running}}' "$container" 2>/dev/null)
+# Define green text color
+GREEN='\033[0;32m'
+# Define reset color (to reset text color to default)
+NC='\033[0m'
 
-	if [[ "status" != "true" ]]; then
-		echo "Starting  $container.."
-		docker start "$container"
-	else
-		echo "$container is already running."
-	fi
-}
-
-#Check if containers are started and active
-echo "[===== Nodes ======]"
-for container in "${containers[@]}";do
-	check_and_start_container "$container"
+echo "[===== Nodes: =====]"
+# Check if container is running, if not start it
+for container in "${containers[@]}"; do
+    # Check if container is running
+    if docker ps --filter "name=$container" | grep -q "$container"; then
+        echo -e "[$container] -${GREEN} Active${NC}"
+    else
+    	# Start the container if not running and discard output
+        docker start "$container" > /dev/null 2>&1 && echo -e "[$container] - ${GREEN}Active${NC}"
+    fi
 done
-
